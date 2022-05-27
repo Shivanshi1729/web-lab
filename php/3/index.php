1856af76bcf48685a694834c0355ab6b35f3d42e
shivanshi <physicist.artist@gmail.com>
2022-05-27 20:55:34 +0530

    php added

---
+++ php/3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!-- 
+Program to maintain session of particular user in different pages. 
+At the end if user wants to logout, then destroy the session. -->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
